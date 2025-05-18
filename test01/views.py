94268f1e77b7db1271d396957a38cc6e063e5261
Abolfazl-Fazeli34 <aboollfazlfazeli34@gmail.com>
2025-05-18 00:17:20 -0230

    make pagination for post_lists 07

--- test01/views.py
+++ test01/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.shortcuts import redirect
-from django.http import HttpResponse, Http404
+# from django.shortcuts import redirect
+from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import *
 
 
@@ -9,8 +10,17 @@ from .models import *
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(posts, 2)
+    page_number = request.GET.get('page', 1)
+    try:
+        page_obj = paginator.page(page_number)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
     context = {
         'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'project/post_list.html', context)
 
